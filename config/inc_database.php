7246838979d4675edd7c74d848bfa732b025b0b5
duanyunchao <duanyunchao@115.com>
2013-11-16 00:17:55 +0800

    基本配置

---
+++ config/inc_database.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 数据库配置
+ * 
+ * @author duanyunchao
+ * @version $Id$
+ */
+$GLOBALS["DATABASE"] = array();
+
+/* my数据库 */
+$GLOBALS['DATABASE']['databases']['db_user']    = 'root';
+$GLOBALS['DATABASE']['databases']['db_pass']    = '123456';
+$GLOBALS['DATABASE']['databases']['db_name']    = 'test';
+$GLOBALS['DATABASE']['databases']['db_charset'] = 'utf-8';
+
+$GLOBALS['DATABASE']['section'] = array();
