9980d477fa9de1f7ed23ac9095b7b52360948a5b
duanyunchao <duanyunchao@115.com>
2013-11-16 01:15:37 +0800

    模板

---
+++ php.dyc.com/control/ctl_index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 主页
+ * 
+ * @author duanyunchao
+ * @version $Id$
+ */
+!defined('IN_INIT') && exit('Access Denied');
+
+class ctl_index
+{
+
+    public function index()
+    {
+        echo "test";
+    }
+
+}
