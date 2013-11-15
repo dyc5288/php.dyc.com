ff7ed42e03f42e539846295ac9cf5640633d24e3
duanyunchao <duanyunchao@115.com>
2013-11-16 01:10:26 +0800

    模板

---
+++ php.dyc.com/control/ctl_test.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 
+ * @author duanyunchao
+ * @version $Id$
+ */
+!defined('IN_INIT') && exit('Access Denied');
+
+class ctl_test
+{
+
+    public function index()
+    {
+        echo "test";
+    }
+
+}
