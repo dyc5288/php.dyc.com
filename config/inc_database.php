bd1655e3ba3e5f75180d3300f70dc83036e77ca4
duanyunchao <duanyunchao@115.com>
2013-11-16 00:21:45 +0800

    基本配置

--- config/inc_database.php
+++ config/inc_database.php
@@ -15,3 +15,9 @@ $GLOBALS['DATABASE']['databases']['db_name']    = 'test';
 $GLOBALS['DATABASE']['databases']['db_charset'] = 'utf-8';
 
 $GLOBALS['DATABASE']['section'] = array();
+$GLOBALS['DATABASE']['section'][0] = array();
+$GLOBALS['DATABASE']['section'][0]['table_name'] = array();
+$GLOBALS['DATABASE']['section'][0]["table_range"] = 1;
+$GLOBALS['DATABASE']['section'][0]["group_count"] = 1;
+$GLOBALS['DATABASE']['section'][0]["ips"][0]["master"] = array("db_host" => "127.0.0.1:3306", "db_name" => "test");  // 默认组
+$GLOBALS['DATABASE']['section'][0]["ips"][0]["slave"][] = array("db_host" => "127.0.0.1:3306", "db_name" => "test"); // 默认组
